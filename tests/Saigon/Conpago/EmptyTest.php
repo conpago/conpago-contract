0a39e3410809435a753e66eb2f38567173166c69
Bartosz Go?ek <bartosz.golek@forcom.com.pl>
2014-11-19 08:13:29 +0100

    Removed Implementation tests Added empty test

---
+++ tests/Saigon/Conpago/EmptyTest.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bgolek
+	 * Date: 2014-11-19
+	 * Time: 08:09
+	 */
+
+	namespace Saigon\Conpago;
+
+
+	class EmptyTest extends \PHPUnit_Framework_TestCase
+	{
+		//technical test for Jenkins Code Coverage plugin
+		function testEmpty()
+		{
+		}
+	}
+ 
