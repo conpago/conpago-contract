9d6f0858455026ed2cabcdbd467277b237f69079
Bartosz Gołek <bartosz.golek@gmail.com>
2015-11-23 11:09:57 +0100

    IPlainPresenter

---
+++ src/Conpago/Presentation/Contract/IPlainPresenter.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: bg
+	 * Date: 13.05.14
+	 * Time: 21:54
+	 */
+
+	namespace Conpago\Presentation\Contract;
+
+	interface IPlainPresenter
+	{
+		function show($data);
+	}
